65a86129a33c6046f239abf268439ad3a8ce9f53
SoudagarLondhe <saudagar1209@gmail.com>
2025-06-03 19:27:04 -0600

    Pattern problem

--- Patterns/Pattern11.java
+++ Patterns/Pattern11.java
@@ -139,3 +139,33 @@ public class Pattern11 {
 //       System.out.println();
 //   }
 
+
+// ----------------------------------------
+// Sample Input :
+//    5
+// Sample Output :
+//  5432*
+//  543*1
+//  54*21
+//  5*321
+//  *4321
+
+// Scanner sc= new Scanner(System.in);
+// int n = sc.nextInt();
+
+// for(int i=1;i<=n;i++)
+// {
+//     for(int j=n;j>=1;j--)
+//     {
+//         if(i==j)
+//         {
+//             System.out.print("*");
+//         }
+//         else
+//         {
+//             System.out.print(j);
+//         }
+        
+//     }
+//     System.out.println();
+// }
