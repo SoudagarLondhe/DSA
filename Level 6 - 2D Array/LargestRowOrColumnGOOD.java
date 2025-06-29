080bdb0c6a0ca6376aa82420e59436790b972f05
SoudagarLondhe <saudagar1209@gmail.com>
2025-06-28 19:47:22 -0600

    Square matrix

---
+++ Level 6 - 2D Array/LargestRowOrColumnGOOD.java
@@ -0,0 +1,88 @@
+Problem statement
+You are given a 2D list (array) with dimensions N rows and M columns, filled with integers. Your task is to find the row or column that has the largest sum of its elements.
+
+Important Rules:
+- If two or more rows/columns have the same sum, choose the one that comes first.
+- If a row and a column have the same largest sum, choose the row.
+Goal: Return which row or column has the largest sum.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= t <= 10^2
+1 <= N <= 10^3
+1 <= M <= 10^3
+Time Limit: 1sec
+Sample Input 1:
+1
+3 2
+6 9 
+8 5 
+9 2 
+Sample Output 1:
+column 0 23
+Sample Input 2:
+1
+4 4
+6 9 8 5 
+9 2 4 1 
+8 3 9 3 
+8 7 8 6 
+Sample Output 2:
+column 0 31
+
+
+
+
+
+
+
+--------------------------------------
+
+
+public class Solution {
+
+	public static void findLargest(int mat[][]){
+		//Your code goes here
+        if (mat.length == 0 || mat[0].length == 0) 
+        {
+            System.out.println("row 0 " + Integer.MIN_VALUE);
+            return;
+        }
+		String str = "row";
+		int max=Integer.MIN_VALUE;
+		int sum;
+		int index=0;
+
+		int[] rowSums = new int[mat.length];
+		for(int j=0;j<mat[0].length;j++)
+		{
+			sum=0;
+			for(int i=0;i<mat.length;i++)
+			{
+				sum=sum+mat[i][j];
+				rowSums[i]=rowSums[i]+mat[i][j];
+			}
+			if(sum > max)
+			{
+				max=sum;
+				index = j;
+				str="column";
+			}
+		}
+
+		for(int k=0;k<rowSums.length;k++)
+		{
+			if(rowSums[k] > max)
+			{
+				max=rowSums[k];
+				str="row";
+				index=k;
+			}
+		}
+
+		System.out.println(str+" "+index+" "+max);
+
+
+	}
+
+}
