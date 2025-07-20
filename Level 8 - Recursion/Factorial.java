5a116cb00f8131abb2d0865417076cda08cd9419
SoudagarLondhe <saudagar1209@gmail.com>
2025-07-20 09:28:53 -0600

    Factorial

---
+++ Level 8 - Recursion/Factorial.java
@@ -0,0 +1,26 @@
+Factorial : 
+n = 4
+24
+
+n=5
+120
+
+
+----------------------
+
+
+class Main {
+    
+    public static int fact(int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        // System.out.println("Try programiz.pro");
+        System.out.println(fact(5));
+    }
+}
