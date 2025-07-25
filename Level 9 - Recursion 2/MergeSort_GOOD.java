885844225e1234de53c733a82469b79d4e96ae52
SoudagarLondhe <saudagar1209@gmail.com>
2025-07-24 21:32:53 -0600

    merge sort

--- Level 9 - Recursion 2/MergeSort_GOOD.java
+++ Level 9 - Recursion 2/MergeSort_GOOD.java
@@ -36,6 +36,75 @@ Constraints :
 
 
 -----------------------------------------
+Prefered :
+
+//updating the array instead of creating everytime new array
+
+public class Solution {
+
+    public static void mergeArray(int[] arr,int l,int mid,int r)
+    {
+        int[] arr1 = new int[mid-l+1];
+        int[] arr2 = new int[r-mid];
+        int i,j,k=l;
+
+        for(i=0,j=l;i<arr1.length;i++,j++)
+        {
+            arr1[i]=arr[j];
+        }
+
+        for(i=0,j=mid+1;i<arr2.length;i++,j++)
+        {
+            arr2[i]=arr[j];
+        }
+
+        i=0;
+        j=0;
+        while(i<arr1.length && j<arr2.length)
+        {
+            if(arr1[i] < arr2[j])
+            {
+                arr[k] = arr1[i];
+                k++;
+                i++;
+            }
+            else 
+            {
+                arr[k] = arr2[j];
+                k++;
+                j++;
+            }
+        }
+
+        while(i<arr1.length)
+        {
+                arr[k] = arr1[i];
+                k++;
+                i++;
+        }
+
+        while(j<arr2.length)
+        {
+                arr[k] = arr2[j];
+                k++;
+                j++;
+        }
+    }
+    public static void mergeSort(int[] arr, int l, int r)
+    {
+        // Write your code here
+        if(l>=r)
+        {
+            return;
+        }
+        int mid = (l+r)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        mergeArray(arr,l,mid,r);
+
+    }
+}
+
 
 --------------------------------------------
 
